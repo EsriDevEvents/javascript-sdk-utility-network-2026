9c641a6d1a71002b7b27e7b159fd50f135119f66
Benjamin Denzler <bdenzler@esri.com>
2026-03-09 13:30:54 -0700

    Move `esriConfig` import

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
 import "./index.css";
 
-import esriConfig from "@arcgis/core/config.js";
-
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
 
+import esriConfig from "@arcgis/core/config.js";
 esriConfig.portalUrl = "https://myHostName.esri.com/arcgis";
 
 createRoot(document.getElementById("root")!).render(
